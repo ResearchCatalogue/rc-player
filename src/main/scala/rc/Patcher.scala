b78a17b2f97f5c96f556b13398bb6915224e6b51
Hanns Holger Rutz <contact@sciss.de>
2015-07-21 12:25:59 +0200

    canvas based patch cord

--- src/main/scala/rc/Patcher.scala
+++ src/main/scala/rc/Patcher.scala
@@ -2,8 +2,9 @@ package rc
 
 import org.scalajs.dom
 import org.scalajs.dom.raw.KeyboardEvent
-import org.scalajs.dom.{DragEvent, FocusEvent, Event, MouseEvent, Element}
+import org.scalajs.dom.{CanvasRenderingContext2D, DragEvent, FocusEvent, Event, MouseEvent, Element}
 import org.scalajs.jquery.{jQuery => $, JQuery}
+import org.w3c.dom.html.HTMLElement
 
 import scala.scalajs.js
 import scalatags.JsDom.all._
@@ -81,7 +82,7 @@ class Patcher extends Widget /* with js.Any */ { patcher =>
       def mkObj(numInlets: Int, numOutlets: Int): Unit = {
         val jq = $(child)
         $(in).removeClass("incomplete")
-        jq.remove("#port")
+        jq.remove(".port")
         var i = 0
         while (i < numInlets) {
           mkPort(jq, i, numInlets, isInlet = true)
@@ -109,12 +110,16 @@ class Patcher extends Widget /* with js.Any */ { patcher =>
         checkWidth(e)
         validate()
       }
-//      in.onblur = { e: FocusEvent =>
-//        println("onblur")
-//        // user left input field
-//        checkWidth(e)
-//        validate()
-//      }
+      in.onblur = { e: FocusEvent =>
+        println("onblur")
+        // XXX TODO -- testing one two testing
+        val objects = $(".obj")
+        println(s"Now there are ${objects.length} objects.")
+        if (objects.length == 2) {
+          mkConnection(objects(0), objects(1))
+        }
+      }
+
       in.oncut      = deferRefresh
       in.onpaste    = deferRefresh
       in.onkeydown  = checkWidth  // cf. http://stackoverflow.com/questions/8795283
@@ -139,5 +144,56 @@ class Patcher extends Widget /* with js.Any */ { patcher =>
     }
 
     elem
+  }
+
+  private def mkConnection(a: dom.html.Element, b: dom.html.Element): Unit = {
+    val aIn   = $(".inlet" , a)
+    val aOut  = $(".outlet", a)
+    val bIn   = $(".inlet" , b)
+    val bOut  = $(".outlet", b)
+
+    if (aOut.length > 0 && bIn.length > 0) {
+      println("Patch a to b")
+      patch(aOut(0), bIn(0))
+    } else if (bOut.length > 0 && aIn.length > 0) {
+      println("Patch b to a")
+      patch(bOut(0), aIn(0))
+    } else {
+      println("Not enough inlets/outlets")
+    }
+  }
+
+  private def patch(source: dom.html.Element, sink: dom.html.Element): Unit = {
+    // div(style := "position: absolute;")
+    val c0 = $(".cables")
+    val c  = if (c0.length > 0) c0.apply(0).asInstanceOf[dom.html.Canvas] else {
+      val tree = canvas(cls := "cables").render
+      tree.width  = 400
+      tree.height = 400
+      $(".patcher").append(tree)
+      tree
+    }
+
+    val sourceP = source.offsetParent.asInstanceOf[dom.html.Element]
+    val sinkP   = sink  .offsetParent.asInstanceOf[dom.html.Element]
+
+//    val parentPos = $(".patcher")(0)
+//    val sourcePos = $(source    )(0)
+//    val sinkPos   = $(sink      ).position().asInstanceOf[js.Dynamic]
+//    val parentX   = parentPos.left.asInstanceOf[Int]
+//    val parentY   = parentPos.top .asInstanceOf[Int]
+    val sourceX   = source.offsetLeft + sourceP.offsetLeft + 4
+    val sourceY   = source.offsetTop  + sourceP.offsetTop  + 1.5
+    val sinkX     = sink  .offsetLeft + sinkP  .offsetLeft + 4
+    val sinkY     = sink  .offsetTop  + sinkP  .offsetTop  + 1.5
+
+    println(s"Patching ($sourceX, $sourceY) -> ($sinkX, $sinkY)")
+
+    val ctx = c.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+    ctx.lineWidth = 1.5
+    ctx.strokeStyle = "rgb(80,  80, 128)"
+    ctx.moveTo(sourceX, sourceY)
+    ctx.lineTo(sinkX  , sinkY  )
+    ctx.stroke()
   }
 }
